bcef4bdcd1f51f2e64e1d2f3b804162fc5cc3cc0
Rabiya <rabiya.noori@gmail.com>
2020-10-10 19:22:12 -0400

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -7,3 +7,24 @@ Created on Sat Oct 10 19:14:15 2020
 """
 
 
+import flask
+app = flask.Flask(__name__, template_folder='templates')
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    if flask.request.method == 'GET':
+        return(flask.render_template('main.html'))
+    if flask.request.method == 'POST':
+        temperature = flask.request.form["exampleFormControlTextarea1"]
+     
+        
+        input_variables = pd.DataFrame([[temperature]],
+                                       columns=['temperature'])
+        
+        prediction = model.predict(input_variables)[0]
+        return flask.render_template('main.html',
+                                     original_input={'Temperature':temperature,
+                                                     'Humidity':humidity,
+                                                     'Windspeed':windspeed},
+                                     result=prediction,
+                                     )
