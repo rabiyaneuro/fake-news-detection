abcbb743e5db5d725754bd80e564ac18a91569f8
Hannah Mei <71854800+hannahmei@users.noreply.github.com>
2020-10-10 20:29:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,28 +2,29 @@
 import flask
 import pickle
 
-app = flask.Flask(__name__, template_folder='templates')
-@app.route('/')
-
-#user input in text 
-def main():
-    return(flask.render_template('main.html'))
-
-
 # Use pickle to load in the pre-trained model.
 with open(f'Downloads/bert.py','t') as f:
      model = pickle.load(f)
+
      
-   
- app.config["DEBUG"] = True
+app = flask.Flask(__name__, template_folder='templates')
+app.config["DEBUG"] = True
+
+@app.route('/', methods=['GET', 'POST'])
+
+#user input in text 
+def main():
+    if flask.request.method == 'GET':
+        return(flask.render_template('main.html'))
+    
+    #if flask.request.method == 'POST':
 
 
- @app.route('/', methods=['GET'])
+app.run()
+
+
+'''
+@app.route('/', methods=['GET'])
 def predict():
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
-
-
-if __name__ == '__main__':
-    app.run()
-
-
+'''
