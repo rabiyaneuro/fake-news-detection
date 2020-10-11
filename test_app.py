c1be82d4ce5e64f2f6f1f362ec304841c06683f5
Rabiya <rabiya.noori@gmail.com>
2020-10-11 01:03:41 -0400

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -4,8 +4,10 @@
 Created on Sat Oct 10 19:14:15 2020
 
 """
-
-
+import torch
+import numpy as np
+from transformers import BertTokenizerFast
+import model.bert
 import flask
 app = flask.Flask(__name__, template_folder='templates')
 
@@ -14,16 +16,37 @@ def main():
     if flask.request.method == 'GET':
         return(flask.render_template('main.html'))
     if flask.request.method == 'POST':
-        temperature = flask.request.form["exampleFormControlTextarea1"]
-     
+        input_text = flask.request.form["exampleFormControlTextarea1"]
+    
         
-        input_variables = pd.DataFrame([[temperature]],
-                                       columns=['temperature'])
+        # predictions
+        max_len = 512
         
-        prediction = model.predict(input_variables)[0]
+        # tokenize and encode sequences in the training set
+        tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased')
+        tokens = tokenizer.batch_encode_plus(
+            input_text,
+            max_length = max_len,
+            padding='max_length',
+            truncation=True
+        )
+        
+        # convert lists to tensors
+        new_seq = torch.tensor(tokens['input_ids'])
+        new_mask = torch.tensor(tokens['attention_mask'])
+        
+        with torch.no_grad():
+          new_preds = model(new_seq, new_mask)
+        
+        prediction = np.argmax(new_preds, axis = 1)
+
         return flask.render_template('main.html',
-                                     original_input={'Temperature':temperature,
-                                                     'Humidity':humidity,
-                                                     'Windspeed':windspeed},
                                      result=prediction,
                                      )
+    
+    
+"""
+
+helpful links:
+    https://blog.cambridgespark.com/deploying-a-machine-learning-model-to-the-web-725688b851c7
+"""
